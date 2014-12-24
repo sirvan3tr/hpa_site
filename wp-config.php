c21faa55ce60ff0de3b2f4dfde4ca44edd78ec67
Sirvan Almasi <sirvanalmasi@macbook-pro.home>
2014-12-24 16:16:50 +0000

    1and1 config files

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,20 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+//define('DB_NAME', 'hpa_db');
 define('DB_NAME', 'hpa_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+//define('DB_USER', 'root');
+define('DB_USER', 'dbo558283544');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+//define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'dbeurobox');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+//define('DB_HOST', 'localhost');
+define('DB_HOST', 'db558283544.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
